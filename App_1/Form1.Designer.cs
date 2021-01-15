d6bdbd7386c6ffbc25a10a320326e6097b841361
Gagilas Danil <danil.gagilas@bk.ru>
2021-01-15 21:01:50 +0700

    Добавьте файлы проекта.

---
+++ App_1/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿
+namespace App_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 12);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(245, 147);
+            this.textBox2.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 165);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(245, 47);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Подсчитать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 218);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(245, 147);
+            this.textBox1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(271, 383);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ App_1/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace App_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int[] counters = new int[10];
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string text = textBox2.Text;
+            char[] temp = text.ToCharArray();
+            string[] tempN = new string[10];
+            foreach (char item in temp)
+            {
+                if (Char.IsNumber(item))
+                {
+                    string next = "\r\n";
+                    switch (item)
+                    {
+                        case '0': counters[0]++; tempN[0] = "0:" + counters[0].ToString() + next; break;
+                        case '1': counters[1]++; tempN[1] = "1:" + counters[1].ToString() + next; break;
+                        case '2': counters[2]++; tempN[2] = "2:" + counters[2].ToString() + next; break;
+                        case '3': counters[3]++; tempN[3] = "3:" + counters[3].ToString() + next; break;
+                        case '4': counters[4]++; tempN[4] = "4:" + counters[4].ToString() + next; break;
+                        case '5': counters[5]++; tempN[5] = "5:" + counters[5].ToString() + next; break;
+                        case '6': counters[6]++; tempN[6] = "6:" + counters[6].ToString() + next; break;
+                        case '7': counters[7]++; tempN[7] = "7:" + counters[7].ToString() + next; break;
+                        case '8': counters[8]++; tempN[8] = "8:" + counters[8].ToString() + next; break;
+                        case '9': counters[9]++; tempN[9] = "9:" + counters[9].ToString() + next; break;
+                    }
+                }
+            }
+            foreach (string item in tempN)
+            {
+                textBox1.Text += item;
+            }
+        }
+    }
+}
